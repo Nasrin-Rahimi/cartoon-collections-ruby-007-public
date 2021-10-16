59d42425a1c0524bbb858d5792a57b748db579f8
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-10-16 11:44:01 -0400

    use enumaratore for solving problemS

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,35 +1,25 @@
-def roll_call_dwarves(dwarfs)# code an argument here
-  # Your code here
-  #dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  dwarfs.each_with_index do | name , index |
-    puts "#{index + 1} #{name}"
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
   end
 end
 
-def summon_captain_planet(veggies)# code an argument here
-  # Your code here
-  # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-    veggies.collect do |call| 
-    call.capitalize + "!"
+def summon_captain_planet(array)
+  array.collect do |element|
+    element.capitalize + "!"
   end
-
 end
 
-def long_planteer_calls(long_planteer_calls)# code an argument here
-  # Your code here
-  answer = false
-  long_planteer_calls.each do |call|
-    if call.length > 4
-      answer = true
-    end
-  end
-    answer
+def long_planteer_calls(array)
+  array.any? {|i| i.length > 4}
 end
 
-def find_the_cheese(cheese)# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
+  #we should have all kind of cheese to see if our ingredients have them or not
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.find do |cheese|
-    cheese_types.include?(cheese)
+
+  ingredients.find do |ingredient|
+    cheese_types.include?(ingredient)
   end
+
 end
